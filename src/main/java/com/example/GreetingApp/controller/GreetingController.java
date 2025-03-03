17114d3682cfa5eab2f4e8d6e2eb58d50c10bf65
kgupta01 <khushinu21@gmail.com>
2025-03-03 22:14:53 +0530

    Added: JSON for different HTTP Methods.

---
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -0,0 +1,44 @@
+package com.example.GreetingApp.controller;
+
+import com.example.GreetingApp.customExceptions.ResourceNotFoundException;
+import com.example.GreetingApp.model.Greeting;
+import com.example.GreetingApp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    @GetMapping
+    public List<Greeting> getGreetings(){
+        return greetingRepository.findAll();
+    }
+
+    @PostMapping
+    public Greeting createGreeting(@RequestBody Greeting greeting){
+        logger.info("Received Greeting: " + greeting);
+        return greetingRepository.save(greeting);
+    }
+
+    @PutMapping("/{id}")
+    public Greeting updateGreeting(@PathVariable Long id, @RequestBody Greeting greetingDetails){
+        logger.info("Updating Greeting with ID: " + id);
+        Greeting greeting = greetingRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Greeting not found with id " + id));
+        greeting.setMessage(greetingDetails.getMessage());
+        return greetingRepository.save(greeting);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGreeting(@PathVariable Long id){
+        greetingRepository.deleteById(id);
+    }
+}
